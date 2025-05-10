22767f85970035559ac3934d252b3ff0144c8fa6
EstivenJaramilloGomez <149891748+Stivenjpa@users.noreply.github.com>
2025-05-10 12:23:28 -0500

    Tarjetas de mascotas
    Se agregan dos actividades para gestionar las tarjetas de mascota, una que las muestra y otra que permite editar los datos, se crea un fragment para las tarjetas y un adaptador para traer la lista de mascotas usando recycler view

--- app/src/main/java/com/example/appandroid/Bienvenida.kt
+++ app/src/main/java/com/example/appandroid/Bienvenida.kt
@@ -5,38 +5,40 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
-import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 
-lateinit var btnsalir:ImageButton
-lateinit var btnRegistrar: Button
+class Bienvenida : AppCompatActivity() {
 
-class Bienvenida : ComponentActivity() {
+    lateinit var btnsalir: ImageButton
+    lateinit var btnRegistrar: Button
+    lateinit var btnPetsFrag: Button
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.layout_bienvenida)
 
-        val emailUsuario = intent.getStringExtra("Correo") //Obtenemos el correo de la actividad "login"
+        val emailUsuario =
+            intent.getStringExtra("Correo") //Obtenemos el correo de la actividad "login"
         val emailTextView = findViewById<TextView>(R.id.textCorreo)
         emailTextView.text = "Usuario: $emailUsuario" //Mostramos el correo
 
         btnsalir = findViewById(R.id.BtnSalir)
         btnRegistrar = findViewById(R.id.bntFormPets)
+        btnPetsFrag = findViewById(R.id.bntFormPets2)
 
-        btnRegistrar.setOnClickListener{
-            val intent = Intent(this, FormPets::class.java) //Salimos a la pantalla "login"
+        btnRegistrar.setOnClickListener {
+            val intent = Intent(this, FormPets::class.java) //Salimos a la pantalla "FormPets"
             startActivity(intent)
         }
 
-
-        btnsalir.setOnClickListener{
-            val intent = Intent(this,Login::class.java) //Salimos a la pantalla "login"
+        btnsalir.setOnClickListener {
+            val intent = Intent(this, Login::class.java) //Salimos a la pantalla "login"
             startActivity(intent)
         }
 
+        btnPetsFrag.setOnClickListener {
+            val intent = Intent(this, PetsTar::class.java) //Salimos a la pantalla "login"
+            startActivity(intent)
         }
-
     }
-
+}
